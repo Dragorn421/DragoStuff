872339dbdd478deb93edb16648261312281d68b7
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-06-14 16:40:34 +0200

    allow zooming out textures in object analyzer

--- Views/OHED/ImageOHEDView.axaml.cs
+++ Views/OHED/ImageOHEDView.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Z64Utils_recreate_avalonia_ui;
 
@@ -7,5 +9,29 @@ public partial class ImageOHEDView : UserControl
     public ImageOHEDView()
     {
         InitializeComponent();
+        ImageWrapperPanel.SizeChanged += (sender, e) => UpdateImageZoom();
+    }
+
+    double _imageZoom = 1;
+
+    protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
+    {
+        _imageZoom *= 1 + e.Delta.Y / 10;
+        if (_imageZoom > 1) _imageZoom = 1;
+        if (_imageZoom < 0.05) _imageZoom = 0.05;
+        UpdateImageZoom();
+    }
+
+    public void UpdateImageZoom()
+    {
+        var fullSize = ImageWrapperPanel.Bounds.Size;
+        var targetSize = new Size(
+            fullSize.Width * _imageZoom,
+            fullSize.Height * _imageZoom
+        );
+        Image.Margin = new(
+            (fullSize.Width - targetSize.Width) / 2,
+            (fullSize.Height - targetSize.Height) / 2
+        );
     }
 }
