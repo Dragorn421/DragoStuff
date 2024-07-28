25975206fcb1d7fc54bd3c5a096bb658c2665fda
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-07-29 00:49:40 +0200

    dotnet new avalonia.app (11.1.0)

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace avalonia_githubaction_build;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
