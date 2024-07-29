cbcae48251f74cc5267fb9babf4e3579c4f91363
Dragorn421 <Dragorn421@users.noreply.github.com>
2024-07-29 16:03:41 +0200

    tentative workaround for windows OpenGL: use WGL for rendering everything (including other controls)

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 
 using Avalonia;
 using System;
+using System.Collections.Generic;
 
 namespace avalonia_glsl_investigate;
 
@@ -18,6 +19,24 @@ class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
+            .With(new Win32PlatformOptions()
+            {
+                RenderingMode = new List<Win32RenderingMode>()
+                {
+                    // Replace using ANGLE with native OpenGL
+                    // However this is "not recommended"
+                    // https://github.com/AvaloniaUI/Avalonia/discussions/6396
+                    // https://github.com/AvaloniaUI/Avalonia/discussions/9393
+                    // A better option would be to keep Avalonia rendering with whatever it wants,
+                    // and use OpenGL to render to a surface that we would then pass back to Avalonia
+                    // https://github.com/AvaloniaUI/Avalonia/discussions/5432
+                    // ? https://github.com/AvaloniaUI/Avalonia/discussions/6842
+                    // ? https://github.com/AvaloniaUI/Avalonia/pull/9639
+                    // https://github.com/AvaloniaUI/Avalonia/discussions/16188
+                    Win32RenderingMode.Wgl,
+                    Win32RenderingMode.Software,
+                }
+            })
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
