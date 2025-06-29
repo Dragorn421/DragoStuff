754d2c35f37a848d28e4d253243adced6eb1e4a8
Dragorn421 <Dragorn421@users.noreply.github.com>
2025-06-29 10:09:58 +0200

    dotnet new avalonia.app

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace avalonia_command_exception;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
